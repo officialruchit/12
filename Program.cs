974d568728b87932cf87e73560eded538d6a7e98
officialruchit <92086127+officialruchit@users.noreply.github.com>
2024-03-13 08:34:47 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace Tax_calculator
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+        }
+        static void RunTest(Utility validator, string name, string dob, object gender, string incomeStr, string investmentStr, string homeLoanStr, string expectedErrorCode, string expectedNonTaxableAmount, string expectedTaxableAmount, string expectedPayableTaxAmount)
+        {
+            string errorCode;
+            int age;
+            decimal income, investment, homeLoan;
+
+            if (!validator.IsValidName(name, out errorCode))
+                Console.WriteLine($"{errorCode}\t");
+            else if (!validator.IsValidDOB(dob, out age, out errorCode))
+                Console.WriteLine($"{errorCode}\t");
+            else if (!validator.IsValidGender(Convert.ToChar(gender), out errorCode))
+                Console.WriteLine($"{errorCode}\t");
+            else if (!validator.IsValidIncome(incomeStr, out income, out errorCode))
+                Console.WriteLine($"{errorCode}\t");
+            else if (!validator.IsValidInvestment(investmentStr, income, out investment, out errorCode))
+                Console.WriteLine($"{errorCode}\t");
+            else if (!validator.IsValidHomeLoan(homeLoanStr, income, investment, out homeLoan, out errorCode))
+                Console.WriteLine($"{errorCode}\t");
+            else
+                Console.WriteLine($"-\t{expectedNonTaxableAmount}\t{expectedTaxableAmount}\t{expectedPayableTaxAmount}");
         }
     }
 }
